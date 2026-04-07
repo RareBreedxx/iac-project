4d2fe5c5d5b02d26e7cb3f9e76f55e92e8f0c584
Yinka Fadoju <o.fadoju@yahoo.com>
2026-04-07 19:51:20 -0400

    Fix ALB port mismatch and enable user_data refresh

--- alb.tf
+++ alb.tf
@@ -34,7 +34,7 @@ resource "aws_lb" "app_lb" {
 # Target Group
 resource "aws_lb_target_group" "tg" {
   name     = "app-target-group"
-  port     = 80
+  port     = 8080
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
 
@@ -64,11 +64,11 @@ resource "aws_lb_listener" "listener" {
 resource "aws_lb_target_group_attachment" "web1" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.web1.id
-  port             = 80
+  port             = 8080
 }
 
 resource "aws_lb_target_group_attachment" "web2" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.web2.id
-  port             = 80
+  port             = 8080
 }
